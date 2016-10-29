518fdf0bb3e80b35a6ce445fdb1e83bf90cfc117
NNCWRMWCWF <frolovav88@mail.ru>
2016-10-29 13:30:05 +0300

    Добавлено четвертое дз

--- homeworks/music_library/bin/music_library.pl
+++ homeworks/music_library/bin/music_library.pl
@@ -2,6 +2,28 @@
 
 use strict;
 use warnings;
+use FindBin;
+use DDP;
+use lib "$FindBin::Bin/../lib";
+use Local::MusicLibrary qw/choice my_sort my_print/;
 
-while (<>) {
+
+my (%hash, $line, @ar, @out_ar);
+my $i = 0;
+
+while(<STDIN>) {
+    chomp;
+    $_ =~ m{^\. /(?<band>[^/]+)/(?<year>\d+)\s+ - \s+(?<album>[^/]+)/(?<track>.+)\.(?<format>[^\.]+)$}x;
+    $hash{$i} = {%+};
+	@ar[$i] = {%+};
+	$i++;
+}
+
+if (@ARGV) {
+	$line = join " ", @ARGV;
+	chomp $line;
+	Local::MusicLibrary::choice(\@ar, $line);
+}
+else {
+	Local::MusicLibrary::my_print(\@ar);
 }
