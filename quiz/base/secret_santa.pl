2df2dbc2b7947ccc0396ee603bfea0261972f672
NNCWRMWCWF <frolovav88@mail.ru>
2016-11-18 22:36:15 +0300

    Добавлено решение задачи secret_santa

--- quiz/base/secret_santa.pl
+++ quiz/base/secret_santa.pl
@@ -1,6 +1,6 @@
 use strict;
 use warnings;
-use Data::Dumper;
+use DDP;
 
 my %has;
 my @ar;
@@ -9,17 +9,26 @@ my $e=0;
 while(my $line = <STDIN>) {
   chomp($line);
   @ar = split/\s+/,$line; 
-  last if($line eq "exit");
+  last if ($line eq "exit");
   for my $w (@ar){
-    $has{$w}=[split/\s+/,$line];}
-  push(@ar1, @ar);
+    @{$has{$w}} = @ar;}
 }
-for (my $i=0; $i<$#ar1; $i++){
-for (my $j=$i+1; $j<$#ar1; $j++){
-$e=0; 
-for my $c (@{$has{$ar1[$i]}}){
-if ($ar1[$j] eq $c){$e++;}}
-if ($e==0){
-print "$ar1[$i]->$ar1[$j]", "\n"; shift @ar1;}
-else {next;}
+p %has;
+
+for my $key (keys %has){
+#print "key $key\n";
+	for my $k (keys %has){
+	#print "k $k\n";
+		my $bol = 1;
+		for my $i (@{$has{$key}}) {
+			#print "i $i\n";
+			if ($k eq $i) { $bol = 0; }}
+			if ($bol) {
+			print "$key -> $k\n"; 
+			push $has{$k}, $key; 
+			for my $key1 (keys %has) {
+			push $has{$key1}, $k;}
+			last;
+			}
 }}
+
